27ca8c46065dea1da956ea7461ebd3b4b0620546
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-01 16:59:01 +1200

    Added User#reviews_voted_for relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   validates :reddit_key, presence: true
+
+  has_many :review_votes
+  has_many :reviews_voted_for, through: :review_votes, source: :review
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe User, :type => :model do
   it { should validate_presence_of :reddit_key }
+  it { should have_many :review_votes }
+  it { should have_many :reviews_voted_for}
 end
