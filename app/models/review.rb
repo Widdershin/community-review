787c69b68f2a59626f2998d8fbd02a5a9b9e765d
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-01 16:33:08 +1200

    Added review#voted_for_by relationship

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ActiveRecord::Base
   validates :name, presence: true
   has_many :review_votes
+  has_many :voted_for_by, through: :review_votes, source: :user
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Review, :type => :model do
   it { should validate_presence_of :name }
   it { should have_many :review_votes }
+  it { should have_many :voted_for_by }
 end
