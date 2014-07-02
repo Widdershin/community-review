78841b2a4709d8c6ec866861bc8fe7cec9a6bc4a
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-02 14:49:16 +1200

    App view now renders review names + test

--- spec/views/app/app.html.erb_spec.rb
+++ spec/views/app/app.html.erb_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "app/app.html.erb", :type => :view do
+  it 'renders reviews' do
+    review = build(:review)
+    assign(:reviews, [review])
+
+    render
+
+    expect(rendered).to include review.name
+  end
 end
