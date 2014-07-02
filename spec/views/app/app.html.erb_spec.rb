0d4bfb78421e74503afc33b50eb73610a4ba65cb
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-02 18:02:32 +1200

    Upvotes only show if logged in

--- spec/views/app/app.html.erb_spec.rb
+++ spec/views/app/app.html.erb_spec.rb
@@ -5,29 +5,35 @@ RSpec.describe "app/app.html.erb", :type => :view do
 
   before do
     assign(:reviews, [review])
+    render
   end
 
   it 'renders reviews' do
-    render
-
     expect(rendered).to include review.name
   end
 
   context 'when logged in' do
     before do
       allow(view).to receive(:current_user).and_return build(:user)
+      render
     end
 
     it 'shows the suggest a review form' do
-      render
       expect(response).to have_selector('form[action="/reviews"]')
+    end
+
+    it 'shows the upvote arrows' do
+      expect(response).to have_selector('.icon-up-open')
     end
   end
 
   context 'when logged out' do
     it 'does not show the suggest a review form' do
-      render
       expect(response).to_not have_selector('form[action="/reviews"]')
+    end
+
+    it 'does not show the upvote arrows' do
+      expect(response).to_not have_selector('.icon-up-open')
     end
   end
 end
