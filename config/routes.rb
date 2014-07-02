005ff7eae5b37a837fb7c3e3228293002cefe62d
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-02 12:57:08 +1200

    Added Review Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resource :reviews, only: [:create, :show]
+
+
   get '/auth/reddit/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#logout'
   root to: 'app#app'
