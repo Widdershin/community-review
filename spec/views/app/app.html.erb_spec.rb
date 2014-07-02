050f86a28bee1793ae9e1db9453ae5d42240d3dc
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-02 16:43:12 +1200

    Added view test to ensure suggestion form is displayed when logged in

--- spec/views/app/app.html.erb_spec.rb
+++ spec/views/app/app.html.erb_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "app/app.html.erb", :type => :view do
-  it 'renders reviews' do
-    review = build(:review)
-    assign(:reviews, [review])
+  let (:review) { build(:review) }
 
+  before do
+    assign(:reviews, [review])
+  end
+
+  it 'renders reviews' do
     render
 
     expect(rendered).to include review.name
+  end
+
+  context 'when logged in' do
+    before do
+      allow(view).to receive(:current_user).and_return build(:user)
+    end
+
+    it 'shows the suggest a review form' do
+      render
+      expect(response).to have_selector('form[action="/reviews"]')
+    end
   end
 end
