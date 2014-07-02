ebdbe8fcb1e616f7e55edf187df4e5dc48653705
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-02 22:31:25 +1200

    Can now only submit a post if logged in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_filter :confirm_logged_in, only: [:create]
+
   def create
     review = Review.create(name: params[:name])
 
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -20,41 +20,50 @@ RSpec.describe ReviewsController, :type => :controller do
   end
 
   describe 'POST create' do
-    let(:user) { build :user }
-    let(:params) { { name: 'test review' } }
+    context 'when logged in' do
+      let(:user) { build :user }
+      let(:params) { { name: 'test review' } }
 
-    before do
-      allow(controller).to receive(:current_user).and_return user
-      allow(Review).to receive(:create).and_return(build :review, name: params[:name])
-    end
+      before do
+        allow(controller).to receive(:current_user).and_return user
+        allow(Review).to receive(:create).and_return(build :review, name: params[:name])
+      end
 
-    it 'creates a review' do
-      expect(Review).to receive(:create).with(name: params[:name])
+      it 'creates a review' do
+        expect(Review).to receive(:create).with(name: params[:name])
 
-      post :create, params
-    end
+        post :create, params
+      end
 
-    context 'params sent as json' do
-      it 'responds with json' do
-        post :create, format: :json, **params
+      context 'params sent as json' do
+        it 'responds with json' do
+          post :create, format: :json, **params
 
-        expected_json = {
-          review: {
-            name: params[:name],
-            id: Fixnum,
-            score: Fixnum
+          expected_json = {
+            review: {
+              name: params[:name],
+              id: Fixnum,
+              score: Fixnum
+            }
           }
-        }
 
-        expect(response.body).to match_json_expression expected_json
+          expect(response.body).to match_json_expression expected_json
+        end
+      end
+
+      context 'params sent form encoded' do
+        it 'redirects to the homepage' do
+          post :create, params
+
+          expect(response).to redirect_to '/'
+        end
       end
     end
 
-    context 'params sent form encoded' do
-      it 'redirects to the homepage' do
-        post :create, params
-
-        expect(response).to redirect_to '/'
+    context 'logged out' do
+      it 'is forbidden' do
+        post :create, name: 'sad face'
+        expect(response).to be_forbidden
       end
     end
   end
