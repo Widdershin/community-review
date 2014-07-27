e5e4e6f80dfbe9a3993c98f2e3e2a3504bca5f7e
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-27 22:12:02 +1200

    Fixed and pruned tests

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,10 +2,10 @@ class VotesController < ApplicationController
   before_filter :confirm_logged_in
 
   def create
-    review = Review.find_by_id(params[:id]) || not_found
+    review = Review.find_by_id params[:id] || not_found
 
     current_user.vote_for_review review
-    p current_user.reviews_voted_for
+
     redirect_to '/'
   end
 end
